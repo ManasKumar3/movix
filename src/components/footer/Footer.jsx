f4f557dbe643f272c86521ec6011c883b17cbe18
Manas Kumar <coolmanas02@gmail.com>
2023-12-14 15:11:51 +0530

    updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,12 +22,10 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                    All the copyrights belong to the original production house.
+                    Contact me at : 
+                    Email: coolmanas02@gmail.com
+                    Ph.No.: 8929453637
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
